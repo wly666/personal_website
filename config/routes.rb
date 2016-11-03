c24470b1217c7ca602bb3ffed47a14960ad526d9
王丽云 <wangliyun@siwei.tech>
2016-11-03 15:42:21 +0800

    配置路由，指定页面起始页

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
+
   devise_for :users
+
   mount Ckeditor::Engine => '/ckeditor'
-  root to: 'articles#index'
+
   resources :article_categories do
     collection do
       get :show_index
     end
   end
+
   resources :articles
+
+  root to: 'article_categories#show_index'
+
 end
